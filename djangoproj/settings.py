66415917773ac4bfa3c7fa8cc18f53d48139eb9d
matintb <matintighbakhsh@gmail.com>
2025-05-30 21:12:52 +0330

    finally managed to add a css style

--- djangoproj/settings.py
+++ djangoproj/settings.py
@@ -114,8 +114,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Example outside project
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), # Example inside project
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
