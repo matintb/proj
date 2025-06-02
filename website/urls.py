98cbb11c6f1c9fcf28297262b86f405b53f61842
matintb <tighbakhshmatin@gmail.com>
2025-06-02 21:27:09 +0330

    changed the .html files to {% %}

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from website.views import index_view,about_view,contact_view
 
+# وقتی پروژه بزرگ باشه ممکنه اسمها شبیه باشه پس از اپ نیم استفاده کردیم ولی اگه تداخل نباشه لازم نیست
+# بعد هم داخل تمپلت بیس {% url 'website:about' %}
+# جنگو ۲ میتونیم به جای این داستان بریم داخل سستینگ و به اپ وبسایت رو اضافه کنیم
+
+app_name = 'website'
+
 urlpatterns = [
     # path(' url ',' view ')
-    path('', index_view),
-    path('about/', about_view),
-    path('contact/', contact_view)
+    path('', index_view, name='index' ),
+    path('about/', about_view, name='about' ),
+    path('contact/', contact_view, name='contact' )
 ]
