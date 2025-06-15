d741124517b57b9bb6675ee2868568436ded4966
matintb <matintighbakhsh@gmail.com>
2025-06-15 16:45:36 +0330

    changed blog admin for better display

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from blog.models import post
 
 # Register your models here.
+@admin.register(post)
+class PostAdmin(admin.ModelAdmin):
+    # dar site admin mitavan bar asas zaman sakht negah kard
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    # faghat title ghabel moshahede bashe dar admin
+    # fields = (title,)
+    # exclude = (title,)
+    list_display = ('title','status','created_date','published_date')
+    list_filter = ('status',)
+    ordering = ('created_date',)
+    search_fields = ('title','content')
+    # ordering = ('-created_date',)
+    # admin.site.register(post,PostAdmin)
