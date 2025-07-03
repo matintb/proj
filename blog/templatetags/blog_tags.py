3e76fb357e33c61a4928c9062661c6fd750d58d5
matintb <tighbakhshmatin@gmail.com>
2025-07-03 20:52:24 +0330

    used inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -10,3 +10,12 @@ register = template.Library()
 def function():
     posts = post.objects.filter(status=1).count()
     return posts
+
+@register.filter
+def snippet(value,arg=15):
+    return value[:arg] + ' ...'
+
+@register.inclusion_tag('popularposts.html')
+def popularposts():
+    posts = post.objects.filter(status=1).order_by('-published_date')[:1]
+    return {'posts':posts}
