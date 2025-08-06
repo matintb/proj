1bd3524a52e0f3655cfa4636fe5e2aab4b2e6867
matintb <matintighbakhsh@gmail.com>
2025-08-06 14:25:32 +0330

    +comments to blog:model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import post,Category
+from blog.models import post,Category,comments
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -22,5 +22,13 @@ class PostAdmin(SummernoteModelAdmin):
     # ordering = ('-created_date',)
     summernote_fields = ('content',)
 
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name','Post','approved','created_date')
+    list_filter = ('Post','approved')
+    search_fields = ('name','Post')
+
+admin.site.register(comments,CommentAdmin)
 admin.site.register(post,PostAdmin)
 admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -42,3 +42,18 @@ class post(models.Model):
         # for /sitemap.xml + add a button in admin/post
     def get_absolute_url(self):
         return reverse('blog:single',kwargs={'pid':self.id})
+    
+class comments(models.Model):
+    Post = models.ForeignKey(post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['created_date']
+        verbose_name = 'نظر'
+        verbose_name_plural = 'نظرات'
